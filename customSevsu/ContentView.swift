4a1c58071ade421bbec479da8c1807ebf80c91fd
igorsazonov <sazonovkerch@gmail.com>
2020-09-05 11:32:06 +0300

    Initial Commit

---
+++ customSevsu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  customSevsu
+//
+//  Created by Игорь Сазонов on 05.09.2020.
+//  Copyright © 2020 Игорь Сазонов. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
